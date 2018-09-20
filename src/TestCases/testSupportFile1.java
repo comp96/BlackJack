ca67b96faade161d81b15f41f1070d4c3a9af9bb
comp96 <computerscience96@outlook.com>
2018-09-19 22:10:26 -0700

    test support data file 1

---
+++ src/TestCases/testSupportFile1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestCases;
+
+import blackjackgame.BlackJackGameFile;
+
+/**
+ *
+ * @author emily
+ */
+public class testSupportFile1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BlackJackGameFile game1;
+        game1 = new BlackJackGameFile("emily", 200 );
+        game1.startFileGame("data1.txt");
+    }
+    
+}
